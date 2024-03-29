607e613b3f91044e5708d8dd19eb53bb86c1d291
OniOlayemi <163363507+OniOlayemi@users.noreply.github.com>
2024-03-29 19:12:56 +0100

    Add files via upload

---
+++ my_calculator.py
@@ -0,0 +1,9 @@
+def add(s, d):
+
+    return s + d
+
+print(add(3,4))
+
+
+def multiply(d, r, s= 3):
+    return d * s * r
---
+++ my_testmodule.py
@@ -0,0 +1,16 @@
+
+
+def a():
+    print('in a')
+
+
+def _b():
+    print('in b')
+
+
+def c():
+    print('in c')
+
+
+def d():
+    print('in d')
